b98d8099b1c5c06285aac49c6492580119eb9dcd
Ankita Mohata <anki15.mohata@gmail.com>
2020-04-12 23:19:21 +0530

    Updates

---
+++ precache-manifest.c6995015ca75ae089329602d650a5118.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e0df63483419050dcae45a116d23cc8",
+    "url": "/Hamburger-react-app/index.html"
+  },
+  {
+    "revision": "e3d147ae9ef033053ae9",
+    "url": "/Hamburger-react-app/static/css/main.3e37f37f.chunk.css"
+  },
+  {
+    "revision": "86a3460265318934c29f",
+    "url": "/Hamburger-react-app/static/js/2.aeb6d4f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hamburger-react-app/static/js/2.aeb6d4f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d147ae9ef033053ae9",
+    "url": "/Hamburger-react-app/static/js/main.eca05f0d.chunk.js"
+  },
+  {
+    "revision": "5b93dcd4a16c92d132d9",
+    "url": "/Hamburger-react-app/static/js/runtime-main.47a7c800.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Hamburger-react-app/static/media/logo.b8503d26.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/burger_builder/precache-manifest.51962feacb1b0617257cdba4e188aac1.js"
+  "/Hamburger-react-app/precache-manifest.c6995015ca75ae089329602d650a5118.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/burger_builder/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Hamburger-react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
