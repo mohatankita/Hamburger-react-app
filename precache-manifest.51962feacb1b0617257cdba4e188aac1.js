6e4f40561dae87a6fc330e24a5c099f867d25b12
Ankita Mohata <anki15.mohata@gmail.com>
2020-04-12 23:09:47 +0530

    Updates

---
+++ precache-manifest.51962feacb1b0617257cdba4e188aac1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78511c0e75eda35a53f4f68ef37d9b77",
+    "url": "/burger_builder/index.html"
+  },
+  {
+    "revision": "4fc29c9565757a6e0c38",
+    "url": "/burger_builder/static/css/main.3e37f37f.chunk.css"
+  },
+  {
+    "revision": "86a3460265318934c29f",
+    "url": "/burger_builder/static/js/2.aeb6d4f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger_builder/static/js/2.aeb6d4f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc29c9565757a6e0c38",
+    "url": "/burger_builder/static/js/main.7239818d.chunk.js"
+  },
+  {
+    "revision": "6ecc3f9016671517f6e7",
+    "url": "/burger_builder/static/js/runtime-main.0a7d1fd9.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger_builder/static/media/logo.b8503d26.png"
+  }
+]);
